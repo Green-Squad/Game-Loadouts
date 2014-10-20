46492e2710497f1cb563fa89fbe8314ee81b1197
Jordan Smith <jordan.r.smith@gmail.com>
2014-10-20 13:39:02 -0400

    stuff we don't remember...

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -326,33 +326,21 @@
             });
         </script>
         @yield('scripts')
-		<script src="{{ asset('js/instantclick.min.js') }}" data-no-instant></script>
-		<script data-no-instant>
 		<!-- Google Analytics -->
         @if(Auth::guest() || Auth::user() -> role != 'Admin')
-            (function(i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] ||
-                function() {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+		<script>
+ 			(function(i,s,o,g,r,a,m) {
+				i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {(i[r].q=i[r].q||[]).push(arguments)},
+				i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})
+			(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-50909901-1', 'gameloadouts.com');
-            ga('require', 'displayfeatures');
+			ga('create', 'UA-50909901-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 
         @endif
-			
-			//InstantClick.on('change', function() {
-			  //ga('send', 'pageview', location.pathname + location.search);
-			//});
- 			
-			//InstantClick.init('mousedown');
-		</script>
         <!-- End Javascript -->
 	
     </body>
