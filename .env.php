b0542a04c53b3ec681146e36693c9076ceb03b24
Kyle Dornblaser <kdornblaser@gmail.com>
2023-11-22 20:20:27 -0500

    add .env.php

---
+++ .env.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'RECAPTCHA_PRIVATE_KEY' => getenv('RECAPTCHA_PRIVATE_KEY'),
+    'DISQUS_SECRET_KEY' => getenv('DISQUS_SECRET_KEY'),
+    'ads' => 0,
+    'hour' => 60,
+    'day' => 1440,
+    'week' => 10080,
+    'month' => 43829,
+    'year' => 525949,
+    'search_url' => getenv('SEARCH_URL')
+);
