49623821122c2086d029017f55f0606c6c2ffd5f
Kyle Dornblaser <kdornblaser@gmail.com>
2024-10-05 19:03:21 -0400

    will this work to force https assets?

--- app/filters.php
+++ app/filters.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
+URL::forceSchema('https');
+
 /*
  * |-------------------------------------------------------------------------- | Application & Route Filters |-------------------------------------------------------------------------- | | Below you will find the "before" and "after" events for the application | which may be used to do any work before or after a request into your | application. Here you may also register your custom route filters. |
  */
